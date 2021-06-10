e7294c0e5046d67c7e2a65a900cb50293388bfdc
Indrek <leede.indrek@gmail.com>
2021-06-10 17:56:55 +0200

    Added Ladder database queries

--- src/router/ladder.ts
+++ src/router/ladder.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import db from '../services/db';
+import { Router } from 'express'
+import db from '../services/db'
 
-export const router = Router();
+export const router = Router()
 
 router.get('/ladder', async function (req, res) {
-  const orderBy = 'experience';
-  const orderDir = req.query.order_dir === 'ASC' ? 'ASC' : 'DESC';
+  const orderBy = 'experience'
+  const orderDir = req.query.order_dir === 'ASC' ? 'ASC' : 'DESC'
 
   const { rows } = await db.query(`
     SELECT
-      characters.name, characters.hero, characters.level, characters.experience::int,
-      characters.hc, characters.start_time, characters.update_time,
+      characters.id, characters.name, characters.hero, characters.level, characters.experience::int,
+      characters.hc, characters.start_time, characters.update_time, characters.in_game_time,
       users.name AS user_name,
       users.country_code AS user_country_code,
       users.dark_color_from AS user_color
@@ -19,13 +19,13 @@ router.get('/ladder', async function (req, res) {
     WHERE d2_mod='D2' AND start_time > 1622494800
     ORDER BY ${orderBy} ${orderDir}
     LIMIT 30
-  `);
+  `)
 
   res.json({
     rows,
     pagination: {
       more: false,
-      offset: rows.length ? rows[rows.length - 1][orderBy] : 0
-    }
-  });
-});
+      offset: rows.length ? rows[rows.length - 1][orderBy] : 0,
+    },
+  })
+})
