2aec30a94b5affcec3bc0116acd4dd6f82f1e31d
gutnar <gutnar@gmail.com>
2021-06-10 19:12:41 +0300

    Fix getCurrentLadder query

--- src/collections/ladders.ts
+++ src/collections/ladders.ts
@@ -4,7 +4,7 @@ import { Ladder } from '../types';
 // Get current ladder
 export async function getCurrentLadder(): Promise<Ladder | null> {
   const time = Math.floor(Date.now()/1000);
-  const { rows } = await db.query(`SELECT * FROM ladders WHERE start_time > $1 AND end_time < $1 ORDER BY id DESC LIMIT 1`, [time]);
+  const { rows } = await db.query(`SELECT * FROM ladders WHERE start_time < $1 AND end_time > $1 ORDER BY id DESC LIMIT 1`, [time]);
 
   if (!rows.length) {
     return null;
