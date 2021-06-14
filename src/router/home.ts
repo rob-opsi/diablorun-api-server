3d5405e38552fc3c4e5475d09ad1f9b8ca9a9826
gutnar <gutnar@gmail.com>
2021-06-14 12:14:17 +0300

    Use outer join speedrun_users in /home

--- src/router/home.ts
+++ src/router/home.ts
@@ -7,16 +7,11 @@ export const router = Router();
 // Get recent speedruns
 router.get('/home', async function (req, res) {
   const latestSpeedruns = await db.query(`
-    WITH pb_runs AS (
-        SELECT
-            *,
-            ROW_NUMBER() OVER(PARTITION BY speedrun_user_id, category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS personal_rank
-        FROM speedruns
-      ), runs AS (
+    WITH runs AS (
         SELECT
           *,
-          RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS category_rank
-        FROM pb_runs WHERE personal_rank=1
+          (RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS category_rank
+        FROM speedruns
       )
       SELECT
           runs.id,
@@ -45,11 +40,11 @@ router.get('/home', async function (req, res) {
           users.dark_color_from AS user_color,
           characters.name AS character_name,
           
-          RANK() OVER (ORDER BY runs.seconds_played ASC) AS rank,
+          (RANK() OVER (ORDER BY runs.seconds_played ASC))::int AS rank,
           runs.category_rank
       FROM runs
-      INNER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
       INNER JOIN speedrun_categories ON runs.category_id = speedrun_categories.id
+      LEFT OUTER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
       LEFT OUTER JOIN users ON runs.user_id = users.id
       LEFT OUTER JOIN characters ON runs.character_id = characters.id
         ORDER BY runs.run_time DESC
@@ -60,12 +55,12 @@ router.get('/home', async function (req, res) {
     WITH pb_runs AS (
         SELECT
             *,
-            ROW_NUMBER() OVER(PARTITION BY speedrun_user_id, category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS personal_rank
+            (ROW_NUMBER() OVER(PARTITION BY COALESCE(speedrun_user_id, user_id::text), category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS personal_rank
         FROM speedruns
       ), runs AS (
         SELECT
           *,
-          RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS category_rank
+          (RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS category_rank
         FROM pb_runs WHERE personal_rank=1
       )
       SELECT
@@ -95,35 +90,35 @@ router.get('/home', async function (req, res) {
           users.dark_color_from AS user_color,
           characters.name AS character_name,
           
-          RANK() OVER (ORDER BY runs.seconds_played ASC) AS rank,
+          (RANK() OVER (ORDER BY runs.seconds_played ASC))::int AS rank,
           runs.category_rank
       FROM runs
-      INNER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
       INNER JOIN speedrun_categories ON runs.category_id = speedrun_categories.id
+      LEFT OUTER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
       LEFT OUTER JOIN users ON runs.user_id = users.id
       LEFT OUTER JOIN characters ON runs.character_id = characters.id
       WHERE runs.category_rank=1 ORDER BY runs.run_time DESC
-      LIMIT 10  
+      LIMIT 10
     `);
   
   const mostMedals = await db.query(`
     WITH pb_runs AS (
       SELECT
           *,
-          ROW_NUMBER() OVER(PARTITION BY speedrun_user_id, category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS personal_rank
+          (ROW_NUMBER() OVER(PARTITION BY COALESCE(speedrun_user_id, user_id::text), category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS personal_rank
       FROM speedruns
   ), runs AS (
       SELECT
           *,
-          RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC) AS category_rank
+          (RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS category_rank
       FROM pb_runs WHERE personal_rank=1
   ), rankings AS (
       SELECT
           runs.speedrun_user_id,
           MAX(runs.user_id) AS user_id,
-          COUNT(*) FILTER(WHERE category_rank=1) AS gold,
-          COUNT(*) FILTER(WHERE category_rank=2) AS silver,
-          COUNT(*) FILTER(WHERE category_rank=3) AS bronze
+          (COUNT(*) FILTER(WHERE category_rank=1))::int AS gold,
+          (COUNT(*) FILTER(WHERE category_rank=2))::int AS silver,
+          (COUNT(*) FILTER(WHERE category_rank=3))::int AS bronze
       FROM runs
       GROUP BY speedrun_user_id ORDER BY gold DESC, silver DESC, bronze DESC
   )
@@ -138,7 +133,7 @@ router.get('/home', async function (req, res) {
       speedrun_users.dark_color_to AS speedrun_user_dark_color_to,
       users.name AS user_name,
       users.dark_color_from AS user_color,
-      RANK() OVER (ORDER BY gold DESC, silver DESC, bronze DESC)
+      (RANK() OVER (ORDER BY gold DESC, silver DESC, bronze DESC))::int
   FROM rankings
   LEFT OUTER JOIN speedrun_users ON rankings.speedrun_user_id = speedrun_users.id
   LEFT OUTER JOIN users ON rankings.user_id = users.id
