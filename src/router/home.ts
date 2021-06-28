7c9a22a7a2914a8dc7ef77352031c57266d20131
gutnar <gutnar@gmail.com>
2021-06-28 11:17:29 +0300

    Fix recently submitted speedruns ranks

--- src/router/home.ts
+++ src/router/home.ts
@@ -7,43 +7,48 @@ export const router = Router();
 // Get recent speedruns
 router.get('/home', async function (req, res) {
   const latestSpeedruns = await db.query(`
-    WITH runs AS (
-        SELECT
-          *,
-          (RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS category_rank
-        FROM speedruns
-      )
+    WITH pb_runs AS (
       SELECT
-          runs.id,
-          runs.user_id,
-          runs.character_id,
-          runs.speedrun_link,
-          runs.run_time,
-          runs.submit_time,
-          runs.category_id,
-          runs.players_category,
-          runs.hc,
-          runs.hero,
-          runs.seconds_played,
-  
-          speedrun_categories.name AS category_name,
-          
-          characters.name AS character_name,
-          COALESCE(users.name, speedrun_users.name) AS user_name,
-          COALESCE(users.country_code, speedrun_users.country_code) AS user_country_code,
-          COALESCE(users.dark_color_from, speedrun_users.dark_color_from) AS user_color,
-          speedrun_users.weblink AS speedrun_user_weblink,
-          
-          (RANK() OVER (ORDER BY runs.seconds_played ASC))::int AS rank,
-          runs.category_rank
-      FROM runs
-      INNER JOIN speedrun_categories ON runs.category_id = speedrun_categories.id
-      LEFT OUTER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
-      LEFT OUTER JOIN users ON runs.user_id = users.id
-      LEFT OUTER JOIN characters ON runs.character_id = characters.id
-        ORDER BY runs.run_time DESC
-      LIMIT 10  
-    `);
+          *,
+          (ROW_NUMBER() OVER(PARTITION BY COALESCE(user_id::text, speedrun_user_id), category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS personal_rank
+      FROM speedruns
+    ), runs AS (
+      SELECT
+        *,
+        (RANK() OVER (PARTITION BY category_id, players_category, hc, hero ORDER BY seconds_played ASC))::int AS category_rank
+      FROM pb_runs WHERE personal_rank=1
+    )
+    SELECT
+        runs.id,
+        runs.user_id,
+        runs.character_id,
+        runs.speedrun_link,
+        runs.run_time,
+        runs.submit_time,
+        runs.category_id,
+        runs.players_category,
+        runs.hc,
+        runs.hero,
+        runs.seconds_played,
+
+        speedrun_categories.name AS category_name,
+        
+        characters.name AS character_name,
+        COALESCE(users.name, speedrun_users.name) AS user_name,
+        COALESCE(users.country_code, speedrun_users.country_code) AS user_country_code,
+        COALESCE(users.dark_color_from, speedrun_users.dark_color_from) AS user_color,
+        speedrun_users.weblink AS speedrun_user_weblink,
+        
+        (RANK() OVER (ORDER BY runs.seconds_played ASC))::int AS rank,
+        runs.category_rank
+    FROM runs
+    INNER JOIN speedrun_categories ON runs.category_id = speedrun_categories.id
+    LEFT OUTER JOIN speedrun_users ON runs.speedrun_user_id = speedrun_users.id
+    LEFT OUTER JOIN users ON runs.user_id = users.id
+    LEFT OUTER JOIN characters ON runs.character_id = characters.id
+      ORDER BY runs.run_time DESC
+    LIMIT 10  
+  `);
   
   const latestRecords = await db.query(`
     WITH pb_runs AS (
